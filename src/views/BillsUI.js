464988da49472ed54a92d601892038c0531f9927
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2021-11-10 21:43:27 +0100

    Bug fixed - [Bug Hunt] Dashboard - correction BillsUI

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,8 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  const orderData = data && data.length ? data.sort((a, b) => new Date(b.date) - new Date(a.date)) : "";
+  return (data && data.length) ? orderData.map(bill => row(bill)).join("") : "";
 }
 
 export default ({ data: bills, loading, error }) => {
